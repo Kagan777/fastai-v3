604b2897b923d74b4205ccd43e29a6cf0735bfb5
Kagan777 <davidkagan@gmail.com>
2019-03-02 18:06:21 +0200

    dropbox url

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1QwDTgcFxK4fz0o1WNaQyGXpW13Tdrqoh'
+export_file_url = 'https://www.dropbox.com/s/npm3umlnfce238c/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['football', 'rogby']
